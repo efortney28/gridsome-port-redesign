38a2d531244204df0b4029ec0e136153f8aba851
Eric Fortney <efortney28@gmail.com>
2020-09-09 15:25:24 -0500

    testing data

--- src/pages/Photography.vue
+++ src/pages/Photography.vue
@@ -1,7 +1,28 @@
 <template>
   <Layout>
+    <b-container fluid class="photos-container">
+      <b-row>
+        <b-col class="photo-entry" v-for="photo in $page.photo.edges" :key="item">
+          <b-img thumbnail :src="photo.node.images"></b-img>
+        </b-col>
+      </b-row>
+    </b-container>
   </Layout>
 </template>
+
+<page-query>
+  query Photo {
+    photo: allPhotoEntry {
+      edges {
+        node {
+          title
+          description
+          images
+        }
+      }
+    }
+  }
+</page-query>
 
 <script>
 export default {
