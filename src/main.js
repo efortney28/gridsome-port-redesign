6e760b54c231fa3dfaa0b59438ff67875f67986b
Eric Fortney <efortney28@gmail.com>
2020-09-09 17:14:06 -0500

    adds lightbox

--- src/main.js
+++ src/main.js
@@ -3,15 +3,18 @@
 
 import DefaultLayout from '~/layouts/Default.vue'
 import BootstrapVue from 'bootstrap-vue'
+import Lightbox2 from 'lightbox2'
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import 'lightbox2/dist/css/lightbox.css'
 
 
 export default function (Vue, { router, head, isClient }) {
   // Set default layout as a global component
   Vue.component('Layout', DefaultLayout)
   Vue.use(BootstrapVue)
+  Vue.use(Lightbox2)
 
   head.link.push({
     rel: 'stylesheet',
--- src/pages/Development.vue
+++ src/pages/Development.vue
@@ -1,7 +1,37 @@
 <template>
   <Layout>
+    <b-container fluid class="photos-container text-center">
+      <div class="info-wrapper">
+        <p class="dev-page-info">Ever since I was little I've been fascinated with anything
+and everything to do with the internet. I remember
+getting a "How to Build Websites: For Dummies" in the
+early 2000's. Ever since then, I've always wanted to
+build websites. Now that I'm older, I taught myself how
+to build anything I or anybody else could want</p>
+      </div>
+      <b-col class="dev-entry" v-for="dev in $page.dev.edges">
+        <h3 class="image-title">{{ dev.node.name}}</h3>
+        <p class="dev-page-info">{{ dev.node.description }}</p>
+        <b-img class="single-entry" thumbnail :src="dev.node.screenshot"></b-img>
+      </b-col>
+    </b-container>
   </Layout>
 </template>
+
+<page-query>
+  query Dev {
+    dev: allDevEntry {
+      edges {
+        node {
+          name
+          url
+          description
+          screenshot
+        }
+      }
+    }
+  }
+</page-query>
 
 <script>
 export default {
@@ -10,5 +40,34 @@ export default {
 </script>
 
 <style>
+.info-wrapper {
+  font-family: "Poppins", sans-serif;
+  color: #EEE;
+  font-weight: 300;
+  font-size: 12px;
+  padding: 1em;
+}
+.photos-container {
+  background: #222831;
+}
 
+.dev-entry {
+  padding: 1rem;
+}
+.single-entry {
+  border: none;
+  background: none;
+}
+.image-title {
+  font-family: "Poppins", sans-serif;
+  color: #EEE;
+  font-weight: 400;
+  font-size: 14px;
+}
+.dev-page-info {
+  font-family: "Poppins", sans-serif;
+  color: #EEE;
+  font-weight: 300;
+  font-size: 12px;
+}
 </style>
