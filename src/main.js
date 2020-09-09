3c00cbb6e9cd2a346adee4c622fa23b9f84e2d48
Eric Fortney <efortney28@gmail.com>
2020-09-09 17:26:59 -0500

    bugs bugs bugs

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import 'lightbox2/dist/css/lightbox.css'
-import 'lightbox2/dist/css/lightbox.js'
+import 'lightbox2/dist/js/lightbox.js'
 
 export default function (Vue, { router, head, isClient }) {
   // Set default layout as a global component
