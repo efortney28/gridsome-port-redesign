da792a5b773275efb667541d84692b28d97f162e
Eric Fortney <efortney28@gmail.com>
2020-09-08 19:51:44 -0500

    netlify integration

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,6 +5,30 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'Gridsome',
-  plugins: []
+  siteName: 'Eric Fortney|Developer|Photographer',
+  siteDescription: 'Photographer and web developer based in the greater Houston, Texas area.',
+  icon: 'src/favicon.png',
+  plugins: [
+    {
+      use: '@gridsome/source-filesystem',
+      options: {
+        path: 'content/photography/**/*.md',
+        typeName: 'PhotoEntry'
+      }
+
+    },
+    {
+      use: '@gridsome/source-filesystem',
+      options: {
+        path: 'content/dev/**/*.md',
+        typeName: 'DevEntry'
+      }
+    },
+    {
+      use: 'gridsome-plugin-netlify-cms',
+      options: {
+        publicPath: '/admin'
+      }
+    },
+  ]
 }
--- src/main.js
+++ src/main.js
@@ -2,8 +2,19 @@
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 
 import DefaultLayout from '~/layouts/Default.vue'
+import BootstrapVue from 'bootstrap-vue'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
 
 export default function (Vue, { router, head, isClient }) {
   // Set default layout as a global component
   Vue.component('Layout', DefaultLayout)
+  Vue.use(BootstrapVue)
+
+  head.link.push({
+    rel: 'stylesheet',
+    href: 'https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;700&display=swap'
+  })
 }
